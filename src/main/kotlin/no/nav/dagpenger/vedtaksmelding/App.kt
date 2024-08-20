e192ad07cad39fae098923deb7cddcf475796f11
Marius Eriksen <marius.eriksen@nav.no>
2024-08-20 14:30:06 +0200

    Logg når app starter, samt debug log ved IllegalAccessException

--- src/main/kotlin/no/nav/dagpenger/vedtaksmelding/App.kt
+++ src/main/kotlin/no/nav/dagpenger/vedtaksmelding/App.kt
@@ -2,10 +2,14 @@ package no.nav.dagpenger.vedtaksmelding
 
 import io.ktor.server.cio.CIO
 import io.ktor.server.engine.embeddedServer
+import mu.KotlinLogging
 import no.nav.dagpenger.vedtaksmelding.helsesjekk.helsesjekker
 import no.nav.dagpenger.vedtaksmelding.vedtaksmelding.meldingOmVedtakApi
 
+private val logger = KotlinLogging.logger {}
+
 fun main() {
+    logger.info { "Starter opp dp-melding-om-vedtak" }
     embeddedServer(CIO, port = 8080) {
         helsesjekker()
         meldingOmVedtakApi()
