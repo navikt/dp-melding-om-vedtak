2bc106d4eeb85c248fd693de8cdbf96f39dfa040
Giao The Cung <giaothe@gmail.com>
2024-08-21 10:49:29 +0200

    POC: hardkode verdi som frontend kan jobbe med
    Co-authored-by: Aurora Christine Hofman <aurora.christine.hofman@nav.no>

--- src/main/kotlin/no/nav/dagpenger/vedtaksmelding/vedtaksmelding/MeldingOmVedtakApi.kt
+++ src/main/kotlin/no/nav/dagpenger/vedtaksmelding/vedtaksmelding/MeldingOmVedtakApi.kt
@@ -1,5 +1,6 @@
 package no.nav.dagpenger.vedtaksmelding.vedtaksmelding
 
+import com.fasterxml.jackson.module.kotlin.readValue
 import io.ktor.http.HttpStatusCode.Companion.OK
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
@@ -7,28 +8,50 @@ import io.ktor.server.auth.authenticate
 import io.ktor.server.response.respond
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
-import no.nav.dagpenger.saksbehandling.api.models.BrevblokkDTO
-import no.nav.dagpenger.saksbehandling.api.models.OpplysningDTO
+import org.intellij.lang.annotations.Language
 
 fun Application.meldingOmVedtakApi() {
     apiConfig()
     routing {
         authenticate("azureAd") {
             get("/melding-om-vedtak/{behandlingId}") {
-                val brevblokkDTO =
-                    BrevblokkDTO(
-                        tekstId = "someTekstId",
-                        opplysninger =
-                            listOf(
-                                OpplysningDTO(
-                                    tekstId = "someOpplysningTekstId",
-                                    verdi = "someVerdi",
-                                ),
-                            ),
-                    )
-                val response = listOf(brevblokkDTO)
-                call.respond(status = OK, message = response)
+                call.respond(status = OK, message = objectMapper.readValue(hubba))
             }
         }
     }
 }
+
+@Language("JSON")
+internal val hubba =
+    """
+    [
+      {
+        "tekstId": "brev.blokk.vedtak-avslag",
+        "opplysninger": [{ "tekstId": "Søknadsdato", "type": "dato", "verdi": "12-05-2024" }]
+      },
+      {
+        "tekstId": "brev.blokk.begrunnelse-avslag-minsteinntekt",
+        "opplysninger": [{ "tekstId": "Søknadsdato", "type": "dato", "verdi": "12-05-2024" }]
+      },
+      {
+        "tekstId": "brev.blokk.rett-til-aa-klaage",
+        "opplysninger": [
+          {
+            "tekstId": "Arbeidsinntekt siste 12 mnd",
+            "type": "penger",
+            "verdi": "0"
+          },
+          { "tekstId": "Inntektskrav for siste 12 mnd", "type": "penger", "verdi": "176000" },
+          {
+            "tekstId": "Arbeidsinntekt siste 36 mnd",
+            "type": "penger",
+            "verdi": "58000"
+          },
+          { "tekstId": "Inntektskrav for siste 36 mnd", "type": "penger", "verdi": "528000" }
+        ]
+      },
+      { "tekstId": "brev.blokk.rett-til-innsyn", "opplysninger": [] },
+      { "tekstId": "brev.blokk.sporsmaal", "opplysninger": [] }
+    ]
+
+    """.trimIndent()
