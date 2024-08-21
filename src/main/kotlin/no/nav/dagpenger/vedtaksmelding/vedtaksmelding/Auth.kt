2b1c0134635111f36853a1ea8f814a1d3c183a82
Giao The Cung <giaothe@gmail.com>
2024-08-21 12:34:00 +0200

    Plumbing kkode for å hente ut token
    Co-authored-by: Aurora Christine Hofman <aurora.christine.hofman@nav.no>

--- src/main/kotlin/no/nav/dagpenger/vedtaksmelding/vedtaksmelding/Auth.kt
+++ src/main/kotlin/no/nav/dagpenger/vedtaksmelding/vedtaksmelding/Auth.kt
@@ -1,16 +1,24 @@
 package no.nav.dagpenger.vedtaksmelding.vedtaksmelding
 
 import com.auth0.jwk.JwkProviderBuilder
+import io.ktor.http.auth.HttpAuthHeader
 import io.ktor.server.auth.AuthenticationConfig
 import io.ktor.server.auth.jwt.JWTAuthenticationProvider
 import io.ktor.server.auth.jwt.JWTCredential
 import io.ktor.server.auth.jwt.JWTPrincipal
 import io.ktor.server.auth.jwt.jwt
+import io.ktor.server.auth.parseAuthorizationHeader
+import io.ktor.server.request.ApplicationRequest
 import mu.KotlinLogging
 import no.nav.dagpenger.vedtaksmelding.Configuration
 import java.net.URL
 
 private val logger = KotlinLogging.logger {}
+
+internal fun ApplicationRequest.jwt(): String =
+    this.parseAuthorizationHeader().let { authHeader ->
+        (authHeader as? HttpAuthHeader.Single)?.blob ?: throw IllegalArgumentException("JWT not found")
+    }
 
 fun AuthenticationConfig.jwt(name: String) {
     jwt(name) {
