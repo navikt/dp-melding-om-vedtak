48f1a5acb4b3c16868e26163c93a9cb552f554df
Marius Eriksen <marius.eriksen@nav.no>
2024-08-30 14:02:45 +0200

    ktlint
    Co-authored-by: Sindre Sægrov <sindre.segrov@nav.no>
    Co-authored-by: Aurora Christine Hofman <aurora.christine.hofman@nav.no>

--- src/main/kotlin/no/nav/dagpenger/vedtaksmelding/Configuration.kt
+++ src/main/kotlin/no/nav/dagpenger/vedtaksmelding/Configuration.kt
@@ -16,7 +16,7 @@ object Configuration {
                 "GRUPPE_SAKSBEHANDLER" to "SaksbehandlerADGruppe",
                 "DP_BEHANDLING_API_SCOPE" to "api://dev-gcp.teamdagpenger.dp-behandling/.default",
                 "DP_BEHANDLING_API_URL" to "http://dp-behandling/behandling",
-                //TODO: SANITY_API_URL sin postfix /development må endres til /prod i produksjon
+                // TODO: SANITY_API_URL sin postfix /development må endres til /prod i produksjon
                 "SANITY_API_URL" to "https://rt6o382n.api.sanity.io/v2021-10-21/data/query/development",
             ),
         )
