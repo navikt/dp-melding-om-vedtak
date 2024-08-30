2e636b5971d70b7b809531bfbbd35b11b1144a82
Marius Eriksen <marius.eriksen@nav.no>
2024-08-30 14:47:16 +0200

    Bruk felles http klient mot sanity og behandling, siden de var likt konfigurert

--- src/main/kotlin/no/nav/dagpenger/vedtaksmelding/vedtaksmelding/Sanity.kt
+++ src/main/kotlin/no/nav/dagpenger/vedtaksmelding/vedtaksmelding/Sanity.kt
@@ -6,28 +6,12 @@ import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.cio.CIO
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
 import io.ktor.client.statement.bodyAsText
-import io.ktor.serialization.jackson.jackson
 import mu.KotlinLogging
 
 private val logger = KotlinLogging.logger { }
-
-internal fun lagHttpKlient(engine: HttpClientEngine): HttpClient {
-    return HttpClient(engine) {
-        expectSuccess = true
-        install(ContentNegotiation) {
-            jackson {
-                registerModule(JavaTimeModule())
-                disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-                configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-            }
-        }
-    }
-}
 
 class Sanity(
     private val sanityUrl: String,
