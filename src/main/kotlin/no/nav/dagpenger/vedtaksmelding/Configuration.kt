d12ec8d45b726e5a14c184ce1344ae83ad64675c
Marius Eriksen <marius.eriksen@nav.no>
2024-08-30 14:15:48 +0200

    debug logg mere mot sanity
    Co-authored-by: Sindre Sægrov <sindre.segrov@nav.no>
    Co-authored-by: Aurora Christine Hofman <aurora.christine.hofman@nav.no>

--- src/main/kotlin/no/nav/dagpenger/vedtaksmelding/Configuration.kt
+++ src/main/kotlin/no/nav/dagpenger/vedtaksmelding/Configuration.kt
@@ -17,7 +17,7 @@ object Configuration {
                 "DP_BEHANDLING_API_SCOPE" to "api://dev-gcp.teamdagpenger.dp-behandling/.default",
                 "DP_BEHANDLING_API_URL" to "http://dp-behandling/behandling",
                 // TODO: SANITY_API_URL sin postfix /development må endres til /prod i produksjon
-                "SANITY_API_URL" to "https://rt6o382n.api.sanity.io/v2021-10-21/data/query/development",
+                "SANITY_API_URL" to "https://rt6o382n.apicdn.sanity.io/v2021-10-21/data/query/development",
             ),
         )
 
