079c86bed33b47256d3ab72b096aa3231431546d
Marius Eriksen <marius.eriksen@nav.no>
2024-09-12 09:59:47 +0200

    Legg brevblokk om spørsmål øverst i faste blokker
    Co-authored-by: Giao The Cung <giaothe@gmail.com>
    Co-authored-by: Sindre Sægrov <sindre.segrov@nav.no>

--- src/main/kotlin/no/nav/dagpenger/vedtaksmelding/model/VedtaksMelding.kt
+++ src/main/kotlin/no/nav/dagpenger/vedtaksmelding/model/VedtaksMelding.kt
@@ -9,9 +9,9 @@ class VedtaksMelding(private val behandling: Behandling, private val mediator: M
     companion object {
         val FASTE_BLOKKER =
             listOf(
+                "brev.blokk.sporsmaal",
                 "brev.blokk.rett-til-aa-klage",
                 "brev.blokk.rett-til-innsyn",
-                "brev.blokk.sporsmaal",
             )
     }
 
