600d0445ee6a9d8bf13f4b999ef1287811bfb304
Marius Eriksen <marius.eriksen@nav.no>
2024-10-17 10:42:51 +0200

    Oppdater server og klient til ktor 3

--- src/main/kotlin/no/nav/dagpenger/vedtaksmelding/MeldingOmVedtakApi.kt
+++ src/main/kotlin/no/nav/dagpenger/vedtaksmelding/MeldingOmVedtakApi.kt
@@ -4,14 +4,13 @@ import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.application.ApplicationCall
-import io.ktor.server.application.call
 import io.ktor.server.auth.authenticate
 import io.ktor.server.request.receiveText
 import io.ktor.server.response.respond
+import io.ktor.server.routing.RoutingContext
 import io.ktor.server.routing.get
 import io.ktor.server.routing.put
 import io.ktor.server.routing.routing
-import io.ktor.util.pipeline.PipelineContext
 import mu.KotlinLogging
 import no.nav.dagpenger.saksbehandling.api.models.MeldingOmVedtakDTO
 import no.nav.dagpenger.saksbehandling.api.models.OpplysningDTO
@@ -58,7 +57,6 @@ fun Application.meldingOmVedtakApi(mediator: Mediator) {
             }
             put("/melding-om-vedtak/{behandlingId}/{brevblokkId}/utvidet-beskrivelse") {
                 requirePlainText()
-
                 val behandlingId = call.parseUUID()
                 val brevblokkId = call.parameters["brevblokkId"].toString()
                 val utvidetBeskrivelseTekst = call.receiveText()
@@ -75,16 +73,17 @@ fun Application.meldingOmVedtakApi(mediator: Mediator) {
     }
 }
 
+private fun RoutingContext.requirePlainText() {
+    val contentTypeHeader = call.request.headers["Content-Type"]
+    require(!contentTypeHeader.isNullOrEmpty() && contentTypeHeader.contains(ContentType.Text.Plain.toString())) {
+        "Content-Type must be ${ContentType.Text.Plain}, but was $contentTypeHeader"
+    }
+}
+
 private fun ApplicationCall.parseSaksbehandler(): Saksbehandler = Saksbehandler(this.request.jwt())
 
 private fun ApplicationCall.parseUUID(): UUID {
     return this.parameters["behandlingId"]?.let {
         UUID.fromString(it)
     } ?: throw IllegalArgumentException("")
-}
-
-private fun PipelineContext<Unit, ApplicationCall>.requirePlainText() {
-    require(call.request.headers["Content-Type"]!!.contains(ContentType.Text.Plain.toString())) {
-        "Content-Type må være ${ContentType.Text.Plain}, men var ${call.request.headers["Content-Type"]}"
-    }
 }
