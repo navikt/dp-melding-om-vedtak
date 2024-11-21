ea4d89daebf121ce136c2587533590dd383b6935
Aurora Christine Hofman <aurora.christine.hofman@nav.no>
2024-11-21 12:25:28 +0100

    Legg til gamle opplysningsnavn for å kunne hente opp eldre behandlinger i produksjon
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>

--- src/main/kotlin/no/nav/dagpenger/vedtaksmelding/model/Opplysning.kt
+++ src/main/kotlin/no/nav/dagpenger/vedtaksmelding/model/Opplysning.kt
@@ -24,7 +24,11 @@ data class Opplysning(
                 "Gjennomsnittlig arbeidsinntekt siste 36 måneder" -> "opplysning.gjennomsnittlig-arbeidsinntekt-siste-36-maaneder"
                 "Brukt beregningsregel" -> "opplysning.brukt-beregningsregel"
                 "Dagsats med barnetillegg etter samordning og 90% regel" -> "opplysning.avrundet-dagsats-med-barnetillegg"
+                // utdatert opplysning må være med pga eldre behandlinger i prod
+                "Avrundet dagsats med barnetillegg" -> "opplysning.avrundet-dagsats-med-barnetillegg"
                 "Samordnet dagsats uten barnetillegg" -> "opplysning.avrundet-dagsats-uten-barnetillegg"
+                // utdatert opplysning må være med pga eldre behandlinger i prod
+                "Avrundet dagsats uten barnetillegg" -> "opplysning.avrundet-dagsats-uten-barnetillegg"
                 "Ukessats med barnetillegg etter samordning" -> "opplysning.ukessats"
                 "Antall stønadsuker" -> "opplysning.antall-stonadsuker"
                 "Egenandel" -> "opplysning.egenandel"
