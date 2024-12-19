85fe2ac94db24de367c1b2887c9bce6843ac389f
Giao The Cung <giaothe@gmail.com>
2024-12-19 14:25:45 +0100

    Fikse tester

--- src/test/kotlin/no/nav/dagpenger/vedtaksmelding/model/VedtaksMeldingTest.kt
+++ src/test/kotlin/no/nav/dagpenger/vedtaksmelding/model/VedtaksMeldingTest.kt
@@ -88,201 +88,201 @@ class VedtaksMeldingTest {
                 ).hentBrevBlokkIder()
             }
         }
+    }
 
-        @Test
-        fun `Rikig brevblokker for avslag på minsteinntekt`() {
-            val minsteinntekt: Opplysning =
-                lagOpplysning(
-                    id = "opplysning.krav-til-minsteinntekt",
-                    verdi = "false",
-                )
-            val opplysninger = setOf(minsteinntekt)
-            val forventedeBrevblokkIder =
-                listOf(
-                    "brev.blokk.vedtak-avslag",
-                    "brev.blokk.begrunnelse-avslag-minsteinntekt",
-                ) + VedtaksMelding.FASTE_BLOKKER
+    @Test
+    fun `Rikig brevblokker for avslag på minsteinntekt`() {
+        val minsteinntekt: Opplysning =
+            lagOpplysning(
+                id = "opplysning.krav-til-minsteinntekt",
+                verdi = "false",
+            )
+        val opplysninger = setOf(minsteinntekt)
+        val forventedeBrevblokkIder =
+            listOf(
+                "brev.blokk.vedtak-avslag",
+                "brev.blokk.begrunnelse-avslag-minsteinntekt",
+            ) + VedtaksMelding.FASTE_BLOKKER
 
-            val mediator =
-                mockk<Mediator>().also {
-                    coEvery { it.hentOpplysningTekstIder(forventedeBrevblokkIder) } returns
-                        listOf(
-                            minsteinntekt.opplysningTekstId,
-                        )
-                }
-            runBlocking {
-                Behandling(
-                    id = UUID.fromString("019145eb-6fbb-769f-b1b1-d2450b383a98"),
-                    tilstand = "Tilstand",
-                    opplysninger = opplysninger,
-                ).let { behandling ->
-                    VedtaksMelding(behandling, mediator).let { vedtaksMelding ->
-                        vedtaksMelding.hentBrevBlokkIder() shouldBe forventedeBrevblokkIder
-                        vedtaksMelding.hentOpplysninger() shouldBe listOf(minsteinntekt)
-                    }
+        val mediator =
+            mockk<Mediator>().also {
+                coEvery { it.hentOpplysningTekstIder(forventedeBrevblokkIder) } returns
+                    listOf(
+                        minsteinntekt.opplysningTekstId,
+                    )
+            }
+        runBlocking {
+            Behandling(
+                id = UUID.fromString("019145eb-6fbb-769f-b1b1-d2450b383a98"),
+                tilstand = "Tilstand",
+                opplysninger = opplysninger,
+            ).let { behandling ->
+                VedtaksMelding(behandling, mediator).let { vedtaksMelding ->
+                    vedtaksMelding.hentBrevBlokkIder() shouldBe forventedeBrevblokkIder
+                    vedtaksMelding.hentOpplysninger() shouldBe listOf(minsteinntekt)
                 }
             }
         }
+    }
 
-        @Test
-        fun `Rikig brevblokker for innvilgelse av ordinære dagpenger uten barn, samordning eller 90 % regel`() {
-            val opplysninger =
-                setOf(minsteinntekt, kravPåDagpengerTrue, ingenBarn, utførtSammordningFalse, nittiProsentRegelIkkeBrukt)
-            val forventedeBrevblokkIder =
-                listOf(
-                    "brev.blokk.vedtak-innvilgelse",
-                    "brev.blokk.hvor-lenge-kan-du-faa-dagpenger",
-                    "brev.blokk.slik-har-vi-beregnet-dagpengene-dine",
-                    "brev.blokk.grunnlag",
-                    "brev.blokk.arbeidstiden-din",
-                    "brev.blokk.egenandel",
-                    "brev.blokk.du-maa-sende-meldekort",
-                    "brev.blokk.utbetaling",
-                    "brev.blokk.husk-aa-sjekke-skattekortet-ditt",
-                    "brev.blokk.vi-stanser-dagpengene-dine-automatisk-naar-du",
-                    "brev.blokk.du-maa-melde-fra-om-endringer",
-                    "brev.blokk.konsekvenser-av-aa-gi-uriktige-eller-mangelfulle-opplysninger",
-                ) + VedtaksMelding.FASTE_BLOKKER
+    @Test
+    fun `Rikig brevblokker for innvilgelse av ordinære dagpenger uten barn, samordning eller 90 % regel`() {
+        val opplysninger =
+            setOf(minsteinntekt, kravPåDagpengerTrue, ingenBarn, utførtSammordningFalse, nittiProsentRegelIkkeBrukt)
+        val forventedeBrevblokkIder =
+            listOf(
+                "brev.blokk.vedtak-innvilgelse",
+                "brev.blokk.hvor-lenge-kan-du-faa-dagpenger",
+                "brev.blokk.slik-har-vi-beregnet-dagpengene-dine",
+                "brev.blokk.grunnlag",
+                "brev.blokk.arbeidstiden-din",
+                "brev.blokk.egenandel",
+                "brev.blokk.du-maa-sende-meldekort",
+                "brev.blokk.utbetaling",
+                "brev.blokk.husk-aa-sjekke-skattekortet-ditt",
+                "brev.blokk.vi-stanser-dagpengene-dine-automatisk-naar-du",
+                "brev.blokk.du-maa-melde-fra-om-endringer",
+                "brev.blokk.konsekvenser-av-aa-gi-uriktige-eller-mangelfulle-opplysninger",
+            ) + VedtaksMelding.FASTE_BLOKKER
 
-            val mediator =
-                mockk<Mediator>()
-            runBlocking {
-                Behandling(
-                    id = UUID.fromString("019145eb-6fbb-769f-b1b1-d2450b383a98"),
-                    tilstand = "Tilstand",
-                    opplysninger = opplysninger,
-                ).let { behandling ->
-                    VedtaksMelding(behandling, mediator).let { vedtaksMelding ->
-                        vedtaksMelding.hentBrevBlokkIder() shouldBe forventedeBrevblokkIder
-                    }
+        val mediator =
+            mockk<Mediator>()
+        runBlocking {
+            Behandling(
+                id = UUID.fromString("019145eb-6fbb-769f-b1b1-d2450b383a98"),
+                tilstand = "Tilstand",
+                opplysninger = opplysninger,
+            ).let { behandling ->
+                VedtaksMelding(behandling, mediator).let { vedtaksMelding ->
+                    vedtaksMelding.hentBrevBlokkIder() shouldBe forventedeBrevblokkIder
                 }
             }
         }
+    }
 
-        @Test
-        fun `Rikig brevblokker for innvilgelse av ordinære dagpenger for person med barn`() {
-            val antallBarn: Opplysning =
-                lagOpplysning(id = "opplysning.antall-barn-som-gir-rett-til-barnetillegg", verdi = "1")
+    @Test
+    fun `Rikig brevblokker for innvilgelse av ordinære dagpenger for person med barn`() {
+        val antallBarn: Opplysning =
+            lagOpplysning(id = "opplysning.antall-barn-som-gir-rett-til-barnetillegg", verdi = "1")
 
-            val opplysninger =
-                setOf(
-                    minsteinntekt,
-                    kravPåDagpengerTrue,
-                    antallBarn,
-                    utførtSammordningFalse,
-                    nittiProsentRegelIkkeBrukt,
-                )
+        val opplysninger =
+            setOf(
+                minsteinntekt,
+                kravPåDagpengerTrue,
+                antallBarn,
+                utførtSammordningFalse,
+                nittiProsentRegelIkkeBrukt,
+            )
 
-            val forventedeBrevblokkIder =
-                listOf(
-                    "brev.blokk.vedtak-innvilgelse",
-                    "brev.blokk.hvor-lenge-kan-du-faa-dagpenger",
-                    "brev.blokk.slik-har-vi-beregnet-dagpengene-dine",
-                    "brev.blokk.barnetillegg",
-                    "brev.blokk.grunnlag",
-                    "brev.blokk.arbeidstiden-din",
-                    "brev.blokk.egenandel",
-                    "brev.blokk.du-maa-sende-meldekort",
-                    "brev.blokk.utbetaling",
-                    "brev.blokk.husk-aa-sjekke-skattekortet-ditt",
-                    "brev.blokk.vi-stanser-dagpengene-dine-automatisk-naar-du",
-                    "brev.blokk.du-maa-melde-fra-om-endringer",
-                    "brev.blokk.konsekvenser-av-aa-gi-uriktige-eller-mangelfulle-opplysninger",
-                ) + VedtaksMelding.FASTE_BLOKKER
+        val forventedeBrevblokkIder =
+            listOf(
+                "brev.blokk.vedtak-innvilgelse",
+                "brev.blokk.hvor-lenge-kan-du-faa-dagpenger",
+                "brev.blokk.slik-har-vi-beregnet-dagpengene-dine",
+                "brev.blokk.barnetillegg",
+                "brev.blokk.grunnlag",
+                "brev.blokk.arbeidstiden-din",
+                "brev.blokk.egenandel",
+                "brev.blokk.du-maa-sende-meldekort",
+                "brev.blokk.utbetaling",
+                "brev.blokk.husk-aa-sjekke-skattekortet-ditt",
+                "brev.blokk.vi-stanser-dagpengene-dine-automatisk-naar-du",
+                "brev.blokk.du-maa-melde-fra-om-endringer",
+                "brev.blokk.konsekvenser-av-aa-gi-uriktige-eller-mangelfulle-opplysninger",
+            ) + VedtaksMelding.FASTE_BLOKKER
 
-            val mediator =
-                mockk<Mediator>()
-            runBlocking {
-                Behandling(
-                    id = UUID.fromString("019145eb-6fbb-769f-b1b1-d2450b383a98"),
-                    tilstand = "Tilstand",
-                    opplysninger = opplysninger,
-                ).let { behandling ->
-                    VedtaksMelding(behandling, mediator).let { vedtaksMelding ->
-                        vedtaksMelding.hentBrevBlokkIder() shouldBe forventedeBrevblokkIder
-                    }
+        val mediator =
+            mockk<Mediator>()
+        runBlocking {
+            Behandling(
+                id = UUID.fromString("019145eb-6fbb-769f-b1b1-d2450b383a98"),
+                tilstand = "Tilstand",
+                opplysninger = opplysninger,
+            ).let { behandling ->
+                VedtaksMelding(behandling, mediator).let { vedtaksMelding ->
+                    vedtaksMelding.hentBrevBlokkIder() shouldBe forventedeBrevblokkIder
                 }
             }
         }
+    }
 
-        @Test
-        fun `Rikig brevblokker for innvilgelse av ordinære dagpenger for person med samordning`() {
-            val utførtSammordningTrue: Opplysning = lagOpplysning(id = "opplysning.har-samordnet", verdi = "true")
+    @Test
+    fun `Rikig brevblokker for innvilgelse av ordinære dagpenger for person med samordning`() {
+        val utførtSammordningTrue: Opplysning = lagOpplysning(id = "opplysning.har-samordnet", verdi = "true")
 
-            val opplysninger =
-                setOf(minsteinntekt, kravPåDagpengerTrue, ingenBarn, utførtSammordningTrue, nittiProsentRegelIkkeBrukt)
+        val opplysninger =
+            setOf(minsteinntekt, kravPåDagpengerTrue, ingenBarn, utførtSammordningTrue, nittiProsentRegelIkkeBrukt)
 
-            val forventedeBrevblokkIder =
-                listOf(
-                    "brev.blokk.vedtak-innvilgelse",
-                    "brev.blokk.hvor-lenge-kan-du-faa-dagpenger",
-                    "brev.blokk.slik-har-vi-beregnet-dagpengene-dine",
-                    "brev.blokk.samordning",
-                    "brev.blokk.grunnlag",
-                    "brev.blokk.arbeidstiden-din",
-                    "brev.blokk.egenandel",
-                    "brev.blokk.du-maa-sende-meldekort",
-                    "brev.blokk.utbetaling",
-                    "brev.blokk.husk-aa-sjekke-skattekortet-ditt",
-                    "brev.blokk.vi-stanser-dagpengene-dine-automatisk-naar-du",
-                    "brev.blokk.du-maa-melde-fra-om-endringer",
-                    "brev.blokk.konsekvenser-av-aa-gi-uriktige-eller-mangelfulle-opplysninger",
-                ) + VedtaksMelding.FASTE_BLOKKER
+        val forventedeBrevblokkIder =
+            listOf(
+                "brev.blokk.vedtak-innvilgelse",
+                "brev.blokk.hvor-lenge-kan-du-faa-dagpenger",
+                "brev.blokk.slik-har-vi-beregnet-dagpengene-dine",
+                "brev.blokk.samordning",
+                "brev.blokk.grunnlag",
+                "brev.blokk.arbeidstiden-din",
+                "brev.blokk.egenandel",
+                "brev.blokk.du-maa-sende-meldekort",
+                "brev.blokk.utbetaling",
+                "brev.blokk.husk-aa-sjekke-skattekortet-ditt",
+                "brev.blokk.vi-stanser-dagpengene-dine-automatisk-naar-du",
+                "brev.blokk.du-maa-melde-fra-om-endringer",
+                "brev.blokk.konsekvenser-av-aa-gi-uriktige-eller-mangelfulle-opplysninger",
+            ) + VedtaksMelding.FASTE_BLOKKER
 
-            val mediator =
-                mockk<Mediator>()
-            runBlocking {
-                Behandling(
-                    id = UUID.fromString("019145eb-6fbb-769f-b1b1-d2450b383a98"),
-                    tilstand = "Tilstand",
-                    opplysninger = opplysninger,
-                ).let { behandling ->
-                    VedtaksMelding(behandling, mediator).let { vedtaksMelding ->
-                        vedtaksMelding.hentBrevBlokkIder() shouldBe forventedeBrevblokkIder
-                    }
+        val mediator =
+            mockk<Mediator>()
+        runBlocking {
+            Behandling(
+                id = UUID.fromString("019145eb-6fbb-769f-b1b1-d2450b383a98"),
+                tilstand = "Tilstand",
+                opplysninger = opplysninger,
+            ).let { behandling ->
+                VedtaksMelding(behandling, mediator).let { vedtaksMelding ->
+                    vedtaksMelding.hentBrevBlokkIder() shouldBe forventedeBrevblokkIder
                 }
             }
         }
+    }
 
-        @Test
-        fun `Rikig brevblokker for innvilgelse av ordinære dagpenger for person med nittiProsentRegel`() {
-            val nittiProsentRegelBrukt: Opplysning =
-                lagOpplysning(
-                    id = "opplysning.andel-av-dagsats-med-barnetillegg-som-overstiger-maks-andel-av-dagpengegrunnlaget",
-                    verdi = 10.toString(),
-                )
+    @Test
+    fun `Rikig brevblokker for innvilgelse av ordinære dagpenger for person med nittiProsentRegel`() {
+        val nittiProsentRegelBrukt: Opplysning =
+            lagOpplysning(
+                id = "opplysning.andel-av-dagsats-med-barnetillegg-som-overstiger-maks-andel-av-dagpengegrunnlaget",
+                verdi = 10.toString(),
+            )
 
-            val opplysninger =
-                setOf(minsteinntekt, kravPåDagpengerTrue, ingenBarn, utførtSammordningFalse, nittiProsentRegelBrukt)
+        val opplysninger =
+            setOf(minsteinntekt, kravPåDagpengerTrue, ingenBarn, utførtSammordningFalse, nittiProsentRegelBrukt)
 
-            val forventedeBrevblokkIder =
-                listOf(
-                    "brev.blokk.vedtak-innvilgelse",
-                    "brev.blokk.hvor-lenge-kan-du-faa-dagpenger",
-                    "brev.blokk.slik-har-vi-beregnet-dagpengene-dine",
-                    "brev.blokk.nittiprosentregel",
-                    "brev.blokk.grunnlag",
-                    "brev.blokk.arbeidstiden-din",
-                    "brev.blokk.egenandel",
-                    "brev.blokk.du-maa-sende-meldekort",
-                    "brev.blokk.utbetaling",
-                    "brev.blokk.husk-aa-sjekke-skattekortet-ditt",
-                    "brev.blokk.vi-stanser-dagpengene-dine-automatisk-naar-du",
-                    "brev.blokk.du-maa-melde-fra-om-endringer",
-                    "brev.blokk.konsekvenser-av-aa-gi-uriktige-eller-mangelfulle-opplysninger",
-                ) + VedtaksMelding.FASTE_BLOKKER
+        val forventedeBrevblokkIder =
+            listOf(
+                "brev.blokk.vedtak-innvilgelse",
+                "brev.blokk.hvor-lenge-kan-du-faa-dagpenger",
+                "brev.blokk.slik-har-vi-beregnet-dagpengene-dine",
+                "brev.blokk.nittiprosentregel",
+                "brev.blokk.grunnlag",
+                "brev.blokk.arbeidstiden-din",
+                "brev.blokk.egenandel",
+                "brev.blokk.du-maa-sende-meldekort",
+                "brev.blokk.utbetaling",
+                "brev.blokk.husk-aa-sjekke-skattekortet-ditt",
+                "brev.blokk.vi-stanser-dagpengene-dine-automatisk-naar-du",
+                "brev.blokk.du-maa-melde-fra-om-endringer",
+                "brev.blokk.konsekvenser-av-aa-gi-uriktige-eller-mangelfulle-opplysninger",
+            ) + VedtaksMelding.FASTE_BLOKKER
 
-            val mediator =
-                mockk<Mediator>()
-            runBlocking {
-                Behandling(
-                    id = UUID.fromString("019145eb-6fbb-769f-b1b1-d2450b383a98"),
-                    tilstand = "Tilstand",
-                    opplysninger = opplysninger,
-                ).let { behandling ->
-                    VedtaksMelding(behandling, mediator).let { vedtaksMelding ->
-                        vedtaksMelding.hentBrevBlokkIder() shouldBe forventedeBrevblokkIder
-                    }
+        val mediator =
+            mockk<Mediator>()
+        runBlocking {
+            Behandling(
+                id = UUID.fromString("019145eb-6fbb-769f-b1b1-d2450b383a98"),
+                tilstand = "Tilstand",
+                opplysninger = opplysninger,
+            ).let { behandling ->
+                VedtaksMelding(behandling, mediator).let { vedtaksMelding ->
+                    vedtaksMelding.hentBrevBlokkIder() shouldBe forventedeBrevblokkIder
                 }
             }
         }
