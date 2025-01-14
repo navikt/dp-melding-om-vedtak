7b681b31698be9dbdf3c0f5ecccba07c2f5a8b8f
Aurora Christine Hofman <aurora.christine.hofman@nav.no>
2025-01-14 08:12:52 +0100

    Slettet ubrukt koe fra E2E

--- src/test/kotlin/no/nav/dagpenger/vedtaksmelding/HtmlE2ETest.kt
+++ src/test/kotlin/no/nav/dagpenger/vedtaksmelding/HtmlE2ETest.kt
@@ -9,29 +9,21 @@ import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpHeaders
 import kotlinx.coroutines.runBlocking
-import no.nav.dagpenger.vedtaksmelding.Configuration.azureAdClient
 import no.nav.dagpenger.vedtaksmelding.k8.setAzureAuthEnv
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import java.util.UUID
 
 internal class HtmlE2ETest {
+    @Disabled
     @Test
     fun `Lage html`() {
         val behandlingId = UUID.fromString("01943b06-1a68-7dad-88e1-19e31cde711c")
 
-        // saksbhehandler token, hentes fra azure-token-generator f,eks
+        // dp-melding-om-vedtak token, hentes fra azure-token-generator f,eks
         @Suppress("ktlint:standard:max-line-length")
         val token =
             ""
-
-        val oboExchanger: (String) -> String by lazy {
-            val scope = "api://dev-gcp.teamdagpenger.dp-melding-om-vedtak/.default"
-            { token: String ->
-                val accessToken = azureAdClient.onBehalfOf(token, scope).accessToken
-                requireNotNull(accessToken) { "Failed to get access token" }
-                accessToken
-            }
-        }
 
         val client =
             HttpClient {
@@ -40,16 +32,12 @@ internal class HtmlE2ETest {
                 }
             }
         setAzureAuthEnv(
-            app = "dp-saksbehandling-frontend",
+            app = "dp-melding-om-vedtak",
             type = "azurerator.nais.io",
         ) {
             runBlocking {
-                val oboToken =
-                    oboExchanger(token).also {
-                        println(it)
-                    }
                 client.post("https://dp-melding-om-vedtak.intern.dev.nav.no/melding-om-vedtak/$behandlingId/html") {
-                    header(HttpHeaders.Authorization, "Bearer $oboToken")
+                    header(HttpHeaders.Authorization, "Bearer $token")
                     header(HttpHeaders.ContentType, "application/json")
                     setBody(
                         """
