7fbb31ed80dc6814b77cfc4b2e31df1a8774aeb3
Aurora Christine Hofman <aurora.christine.hofman@nav.no>
2025-01-14 08:32:41 +0100

    Bruk data fra requestbody inn i html meldingsgenerering

--- src/test/kotlin/no/nav/dagpenger/vedtaksmelding/model/AvslagHtmlTest.kt
+++ src/test/kotlin/no/nav/dagpenger/vedtaksmelding/model/AvslagHtmlTest.kt
@@ -2,6 +2,9 @@ package no.nav.dagpenger.vedtaksmelding.model
 
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
+import no.nav.dagpenger.saksbehandling.api.models.BehandlerDTO
+import no.nav.dagpenger.saksbehandling.api.models.BehandlerEnhetDTO
+import no.nav.dagpenger.saksbehandling.api.models.MeldingOmVedtakDataDTO
 import no.nav.dagpenger.vedtaksmelding.Configuration
 import no.nav.dagpenger.vedtaksmelding.Mediator
 import no.nav.dagpenger.vedtaksmelding.portabletext.HtmlConverter
@@ -18,6 +21,33 @@ class AvslagHtmlTest {
 
     @Test
     fun hubba() {
+        val meldingOmVedtakData =
+            MeldingOmVedtakDataDTO(
+                fornavn = "Test ForNavn",
+                etternavn = "Test EtterNavn",
+                fodselsnummer = "12345678901",
+                sakId = "sak123",
+                saksbehandler =
+                    BehandlerDTO(
+                        fornavn = "Ola",
+                        etternavn = "Nordmann",
+                        enhet =
+                            BehandlerEnhetDTO(
+                                navn = "Enhet Navn",
+                                postadresse = "Postadresse 123",
+                            ),
+                    ),
+                beslutter =
+                    BehandlerDTO(
+                        fornavn = "Kari",
+                        etternavn = "Nordmann",
+                        enhet =
+                            BehandlerEnhetDTO(
+                                navn = "Enhet Navn",
+                                postadresse = "Postadresse 123",
+                            ),
+                    ),
+            )
         val avslag =
             Avslag(
                 vedtak = vedtak,
@@ -31,7 +61,7 @@ class AvslagHtmlTest {
         runBlocking {
             avslag.hentOpplysninger()
             val brevBlokker = avslag.hentBrevBlokker()
-            writeStringToFile(content = HtmlConverter.toHtml(brevBlokker, avslag.hentOpplysninger()))
+            writeStringToFile(content = HtmlConverter.toHtml(brevBlokker, avslag.hentOpplysninger(), meldingOmVedtakData))
         }
     }
 
