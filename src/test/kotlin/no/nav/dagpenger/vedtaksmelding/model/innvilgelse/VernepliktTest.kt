e7540a4f37ad05e362b058f2373e5d95f2928112
Marius Eriksen <marius.eriksen@nav.no>
2025-01-17 08:57:52 +0100

    Legg til ekstra brevblokk om at det er gunstigere for bruker å få satsen til verneplikt enn ordinære dagpenger.
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>

--- src/test/kotlin/no/nav/dagpenger/vedtaksmelding/model/innvilgelse/VernepliktTest.kt
+++ src/test/kotlin/no/nav/dagpenger/vedtaksmelding/model/innvilgelse/VernepliktTest.kt
@@ -1,21 +1,20 @@
-package no.nav.dagpenger.vedtaksmelding.model
+package no.nav.dagpenger.vedtaksmelding.model.innvilgelse
 
 import io.kotest.matchers.shouldBe
+import no.nav.dagpenger.vedtaksmelding.model.Opplysning
 import no.nav.dagpenger.vedtaksmelding.model.Opplysning.Datatype.BOOLSK
 import no.nav.dagpenger.vedtaksmelding.model.Opplysning.Datatype.FLYTTALL
 import no.nav.dagpenger.vedtaksmelding.model.Opplysning.Datatype.HELTALL
 import no.nav.dagpenger.vedtaksmelding.model.Opplysning.Enhet.KRONER
 import no.nav.dagpenger.vedtaksmelding.model.Opplysning.Enhet.UKER
+import no.nav.dagpenger.vedtaksmelding.model.VedtakMapper
 import org.junit.jupiter.api.Test
 
-class InnvilgelseVernepliktVedtakMapperTest {
+class VernepliktTest {
     private val resourceRetriever = object {}.javaClass
+
     private val vedtakVerneplikt =
         resourceRetriever.getResource("/json/vedtak-verneplikt.json")?.let { VedtakMapper(it.readText()).vedtak() }
-            ?: throw RuntimeException("Fant ikke ressurs")
-
-    private val vedtakVernepliktOgInntekt =
-        resourceRetriever.getResource("/json/vedtak-verneplikt-og-inntekt.json")?.let { VedtakMapper(it.readText()).vedtak() }
             ?: throw RuntimeException("Fant ikke ressurs")
 
     @Test
@@ -26,6 +25,17 @@ class InnvilgelseVernepliktVedtakMapperTest {
                 verdi = "3.0",
                 datatype = FLYTTALL,
                 enhet = KRONER,
+            )
+    }
+
+    @Test
+    fun `Hent opplysning Antall stønadsuker som gis ved ordinære dagpenger`() {
+        vedtakVerneplikt.finnOpplysning("opplysning.antall-stonadsuker-som-gis-ved-ordinare-dagpenger") shouldBe
+            Opplysning(
+                opplysningTekstId = "opplysning.antall-stonadsuker-som-gis-ved-ordinare-dagpenger",
+                verdi = "52",
+                datatype = HELTALL,
+                enhet = UKER,
             )
     }
 
@@ -43,34 +53,6 @@ class InnvilgelseVernepliktVedtakMapperTest {
                 opplysningTekstId = "opplysning.er-innvilget-med-verneplikt",
                 verdi = true.toString(),
                 datatype = BOOLSK,
-            )
-    }
-
-    @Test
-    fun `skal hente opplysning om innvilgelse av verneplikt og riktig antall stonadsuker når bruker også oppfyller inntektskravet`() {
-        vedtakVernepliktOgInntekt.finnOpplysning("opplysning.antall-stonadsuker") shouldBe
-            Opplysning(
-                opplysningTekstId = "opplysning.antall-stonadsuker",
-                verdi = "26",
-                datatype = HELTALL,
-                enhet = UKER,
-            )
-        vedtakVernepliktOgInntekt.finnOpplysning("opplysning.er-innvilget-med-verneplikt") shouldBe
-            Opplysning(
-                opplysningTekstId = "opplysning.er-innvilget-med-verneplikt",
-                verdi = true.toString(),
-                datatype = BOOLSK,
-            )
-    }
-
-    @Test
-    fun `Hent opplysning Antall stønadsuker som gis ved ordinære dagpenger`() {
-        vedtakVerneplikt.finnOpplysning("opplysning.antall-stonadsuker-som-gis-ved-ordinare-dagpenger") shouldBe
-            Opplysning(
-                opplysningTekstId = "opplysning.antall-stonadsuker-som-gis-ved-ordinare-dagpenger",
-                verdi = "52",
-                datatype = HELTALL,
-                enhet = UKER,
             )
     }
 }
