b05e01051e8c443aeb9aac5f5dec92deda7c6c4d
Giao The Cung <giaothe@gmail.com>
2025-01-17 12:43:19 +0100

    Upgrade logback core
    Co-authored-by: Aurora Christine Hofman <aurora.christine.hofman@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 dependencies {
     implementation(project(":openapi"))
-    implementation("ch.qos.logback:logback-classic:1.5.7")
+    implementation("ch.qos.logback:logback-classic:1.5.13")
     implementation("net.logstash.logback:logstash-logback-encoder:8.0")
     implementation("com.fasterxml.uuid:java-uuid-generator:5.1.0")
     implementation(libs.kotlin.logging)
