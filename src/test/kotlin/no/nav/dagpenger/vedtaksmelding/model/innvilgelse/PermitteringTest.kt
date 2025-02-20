0178d9e18ff3e72c1fc4c7dae8f57092e7640053
Mona Kjeldsrud <mona.kjeldsrud@nav.no>
2025-02-20 08:31:35 +0100

    Splittet opp testene for å unngå for store testklasser.

--- src/test/kotlin/no/nav/dagpenger/vedtaksmelding/model/innvilgelse/PermitteringTest.kt
+++ src/test/kotlin/no/nav/dagpenger/vedtaksmelding/model/innvilgelse/PermitteringTest.kt
@@ -4,9 +4,31 @@ import io.kotest.matchers.shouldBe
 import no.nav.dagpenger.vedtaksmelding.model.OpplysningTyper.AntallPermitteringsuker
 import no.nav.dagpenger.vedtaksmelding.model.OpplysningTyper.AntallStønadsuker
 import no.nav.dagpenger.vedtaksmelding.model.VedtakMapper
+import no.nav.dagpenger.vedtaksmelding.model.VedtakMelding
+import no.nav.dagpenger.vedtaksmelding.model.VilkårTyper.OPPFYLLER_KRAVET_TIL_PERMITTERING
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_ARBEIDSFORHOLD_AVSLUTT_PERMITTERT
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_ARBEIDSTIDEN_DIN
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_DAGPENGEPERIODE_PERMITTERT
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_EGENANDEL
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_GRUNNLAG
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_HVA_SKJER_ETTER_PERMITTERINGEN
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_INNLEDNING_PERMITTERT
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_KONSEKVENSER_FEILOPPLYSNING
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_MELDEKORT
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_MELD_FRA_OM_ENDRINGER
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_SKATTEKORT
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_SLIK_HAR_VI_BEREGNET_DAGPENGENE_DINE
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_STANS_ÅRSAKER
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_UTBETALING
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_VIRKNINGSDATO_BEGRUNNELSE_PERMITTERT
 import no.nav.dagpenger.vedtaksmelding.model.vedtak.Opplysning
 import no.nav.dagpenger.vedtaksmelding.model.vedtak.Opplysning.Datatype.HELTALL
 import no.nav.dagpenger.vedtaksmelding.model.vedtak.Opplysning.Enhet.UKER
+import no.nav.dagpenger.vedtaksmelding.model.vedtak.Vedtak
+import no.nav.dagpenger.vedtaksmelding.model.vedtak.Vedtak.Utfall
+import no.nav.dagpenger.vedtaksmelding.model.vedtak.Vilkår
+import no.nav.dagpenger.vedtaksmelding.model.vedtak.Vilkår.Status.OPPFYLT
+import no.nav.dagpenger.vedtaksmelding.uuid.UUIDv7
 import org.junit.jupiter.api.Test
 
 class PermitteringTest {
@@ -29,6 +51,46 @@ class PermitteringTest {
                 datatype = HELTALL,
                 enhet = UKER,
             )
+    }
+
+    @Test
+    fun `Rikig brevblokker rekkefølge for innvilgelse med permittering`() {
+        val forventedeBrevblokkIder =
+            listOf(
+                INNVILGELSE_INNLEDNING_PERMITTERT.brevblokkId,
+                INNVILGELSE_VIRKNINGSDATO_BEGRUNNELSE_PERMITTERT.brevblokkId,
+                INNVILGELSE_DAGPENGEPERIODE_PERMITTERT.brevblokkId,
+                INNVILGELSE_ARBEIDSFORHOLD_AVSLUTT_PERMITTERT.brevblokkId,
+                INNVILGELSE_HVA_SKJER_ETTER_PERMITTERINGEN.brevblokkId,
+                INNVILGELSE_SLIK_HAR_VI_BEREGNET_DAGPENGENE_DINE.brevblokkId,
+                INNVILGELSE_GRUNNLAG.brevblokkId,
+                INNVILGELSE_ARBEIDSTIDEN_DIN.brevblokkId,
+                INNVILGELSE_EGENANDEL.brevblokkId,
+                INNVILGELSE_MELDEKORT.brevblokkId,
+                INNVILGELSE_UTBETALING.brevblokkId,
+                INNVILGELSE_SKATTEKORT.brevblokkId,
+                INNVILGELSE_STANS_ÅRSAKER.brevblokkId,
+                INNVILGELSE_MELD_FRA_OM_ENDRINGER.brevblokkId,
+                INNVILGELSE_KONSEKVENSER_FEILOPPLYSNING.brevblokkId,
+            ) + VedtakMelding.fasteAvsluttendeBlokker
+
+        InnvilgelseMelding(
+            vedtak =
+                Vedtak(
+                    behandlingId = UUIDv7.ny(),
+                    vilkår =
+                        setOf(
+                            Vilkår(
+                                navn = OPPFYLLER_KRAVET_TIL_PERMITTERING.vilkårNavn,
+                                status = OPPFYLT,
+                            ),
+                        ),
+                    utfall = Utfall.INNVILGET,
+                    opplysninger = setOf(),
+                    fagsakId = "fagsakId test",
+                ),
+            alleBrevblokker = emptyList(),
+        ).brevBlokkIder() shouldBe forventedeBrevblokkIder
     }
 }
 
--- src/test/kotlin/no/nav/dagpenger/vedtaksmelding/model/innvilgelse/VernepliktTest.kt
+++ src/test/kotlin/no/nav/dagpenger/vedtaksmelding/model/innvilgelse/VernepliktTest.kt
@@ -6,11 +6,28 @@ import no.nav.dagpenger.vedtaksmelding.model.OpplysningTyper.AntallStønadsuker
 import no.nav.dagpenger.vedtaksmelding.model.OpplysningTyper.AntallStønadsukerSomGisVedOrdinæreDagpenger
 import no.nav.dagpenger.vedtaksmelding.model.OpplysningTyper.ErInnvilgetMedVerneplikt
 import no.nav.dagpenger.vedtaksmelding.model.VedtakMapper
+import no.nav.dagpenger.vedtaksmelding.model.VedtakMelding
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_ARBEIDSTIDEN_DIN_VERNEPLIKT
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_DAGPENGEPERIODE_VERNEPLIKT
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_EGENANDEL
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_GRUNNLAG_VERNEPLIKT
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_INNLEDNING
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_KONSEKVENSER_FEILOPPLYSNING
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_MELDEKORT
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_MELD_FRA_OM_ENDRINGER
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_SKATTEKORT
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_SLIK_HAR_VI_BEREGNET_DAGPENGENE_DINE
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_STANS_ÅRSAKER
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_UTBETALING
+import no.nav.dagpenger.vedtaksmelding.model.innvilgelse.InnvilgelseBrevblokker.INNVILGELSE_VIRKNINGSDATO_BEGRUNNELSE
 import no.nav.dagpenger.vedtaksmelding.model.vedtak.Opplysning
 import no.nav.dagpenger.vedtaksmelding.model.vedtak.Opplysning.Datatype.BOOLSK
 import no.nav.dagpenger.vedtaksmelding.model.vedtak.Opplysning.Datatype.FLYTTALL
 import no.nav.dagpenger.vedtaksmelding.model.vedtak.Opplysning.Datatype.HELTALL
 import no.nav.dagpenger.vedtaksmelding.model.vedtak.Opplysning.Enhet.UKER
+import no.nav.dagpenger.vedtaksmelding.model.vedtak.Vedtak
+import no.nav.dagpenger.vedtaksmelding.model.vedtak.Vedtak.Utfall
+import no.nav.dagpenger.vedtaksmelding.uuid.UUIDv7
 import org.junit.jupiter.api.Test
 
 class VernepliktTest {
@@ -21,7 +38,7 @@ class VernepliktTest {
             ?: throw RuntimeException("Fant ikke ressurs")
 
     @Test
-    fun `skal hente antall G som gis som grunnlag ved bruk av vernepliktregel`() {
+    fun `Hent opplysning om antall G som gis som grunnlag ved bruk av vernepliktregel`() {
         vedtakVerneplikt.finnOpplysning(AntallGSomGisSomGrunnlagVedVerneplikt.opplysningTekstId) shouldBe
             Opplysning(
                 opplysningTekstId = AntallGSomGisSomGrunnlagVedVerneplikt.opplysningTekstId,
@@ -33,7 +50,7 @@ class VernepliktTest {
     }
 
     @Test
-    fun `Hent opplysning Antall stønadsuker som gis ved ordinære dagpenger`() {
+    fun `Hent opplysning om antall stønadsuker som gis ved ordinære dagpenger`() {
         vedtakVerneplikt.finnOpplysning(AntallStønadsukerSomGisVedOrdinæreDagpenger.opplysningTekstId) shouldBe
             Opplysning(
                 opplysningTekstId = AntallStønadsukerSomGisVedOrdinæreDagpenger.opplysningTekstId,
@@ -46,19 +63,57 @@ class VernepliktTest {
     }
 
     @Test
-    fun `skal hente opplysning om innvilgelse av verneplikt og riktig antall stonadsuker`() {
-        vedtakVerneplikt.finnOpplysning(AntallStønadsuker.opplysningTekstId) shouldBe
-            Opplysning(
-                opplysningTekstId = AntallStønadsuker.opplysningTekstId,
-                råVerdi = "26",
-                datatype = HELTALL,
-                enhet = UKER,
-            )
+    fun `Hent opplysning som forteller om vedtak er innvilget med vernepliktregel`() {
         vedtakVerneplikt.finnOpplysning(ErInnvilgetMedVerneplikt.opplysningTekstId) shouldBe
             Opplysning(
                 opplysningTekstId = ErInnvilgetMedVerneplikt.opplysningTekstId,
                 råVerdi = true.toString(),
                 datatype = BOOLSK,
             )
+    }
+
+    @Test
+    fun `Hent opplysning om antall stønadsuker`() {
+        vedtakVerneplikt.finnOpplysning(AntallStønadsuker.opplysningTekstId) shouldBe
+            Opplysning(
+                opplysningTekstId = AntallStønadsuker.opplysningTekstId,
+                råVerdi = "26",
+                datatype = HELTALL,
+                enhet = UKER,
+            ).also {
+                it.formatertVerdi shouldBe "26"
+            }
+    }
+
+    @Test
+    fun `Rikig brevblokker for innvilgelse av dagpenger etter verneplikt`() {
+        val forventedeBrevblokkIder =
+            listOf(
+                INNVILGELSE_INNLEDNING.brevblokkId,
+                INNVILGELSE_VIRKNINGSDATO_BEGRUNNELSE.brevblokkId,
+                INNVILGELSE_DAGPENGEPERIODE_VERNEPLIKT.brevblokkId,
+                INNVILGELSE_SLIK_HAR_VI_BEREGNET_DAGPENGENE_DINE.brevblokkId,
+                INNVILGELSE_GRUNNLAG_VERNEPLIKT.brevblokkId,
+                INNVILGELSE_ARBEIDSTIDEN_DIN_VERNEPLIKT.brevblokkId,
+                INNVILGELSE_EGENANDEL.brevblokkId,
+                INNVILGELSE_MELDEKORT.brevblokkId,
+                INNVILGELSE_UTBETALING.brevblokkId,
+                INNVILGELSE_SKATTEKORT.brevblokkId,
+                INNVILGELSE_STANS_ÅRSAKER.brevblokkId,
+                INNVILGELSE_MELD_FRA_OM_ENDRINGER.brevblokkId,
+                INNVILGELSE_KONSEKVENSER_FEILOPPLYSNING.brevblokkId,
+            ) + VedtakMelding.fasteAvsluttendeBlokker
+
+        InnvilgelseMelding(
+            vedtak =
+                Vedtak(
+                    behandlingId = UUIDv7.ny(),
+                    vilkår = emptySet(),
+                    utfall = Utfall.INNVILGET,
+                    opplysninger = setOf(Opplysning(ErInnvilgetMedVerneplikt.opplysningTekstId, "true", BOOLSK)),
+                    fagsakId = "fagsakId test",
+                ),
+            alleBrevblokker = emptyList(),
+        ).brevBlokkIder() shouldBe forventedeBrevblokkIder
     }
 }
