2671c3db54cfabc013709f295c6bfbf4a9db90c6
Aurora Christine Hofman <aurora.christine.hofman@nav.no>
2025-05-19 09:22:35 +0200

    Start logikk for klagebrev opprettholdelse og avvist. Lagt til formkravopplysninger

--- src/main/kotlin/no/nav/dagpenger/vedtaksmelding/model/KlageOpplysningTyper.kt
+++ src/main/kotlin/no/nav/dagpenger/vedtaksmelding/model/KlageOpplysningTyper.kt
@@ -5,4 +5,24 @@ enum class KlageOpplysningTyper(val opplysningNavnId: String, val opplysningTeks
         opplysningNavnId = "KLAGE_MOTTATT",
         opplysningTekstId = "opplysning.klage-mottatt-dato",
     ),
+    KlageUtfall(
+        opplysningNavnId = "UTFALL",
+        opplysningTekstId = "opplysning.klage-utfall",
+    ),
+    ErKlagenSkriftelig(
+        opplysningNavnId = "ER_KLAGEN_SKRIFTLIG",
+        opplysningTekstId = "opplysning.er-klagen-skriftlig",
+    ),
+    ErKlagenUnderskrevet(
+        opplysningNavnId = "ER_KLAGEN_UNDERSKREVET",
+        opplysningTekstId = "opplysning.er-klagen-underskrevet",
+    ),
+    KlagenNevnerEndring(
+        opplysningNavnId = "KLAGEN_NEVNER_ENDRING",
+        opplysningTekstId = "opplysning.klagen-nevner-endring",
+    ),
+    RettsligKlageinteresse(
+        opplysningNavnId = "RETTSLIG_KLAGEINTERESSE",
+        opplysningTekstId = "opplysning.rettsleg-klageinteresse",
+    ),
 }
