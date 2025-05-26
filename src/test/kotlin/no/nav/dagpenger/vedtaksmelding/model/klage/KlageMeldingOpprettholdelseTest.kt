631a26b08ce6dc1561977851210ddf0187f9ac34
Mona Kjeldsrud <mona.kjeldsrud@nav.no>
2025-05-26 15:28:45 +0200

    Lagt til faste blokker på opprettholdelse også

--- src/test/kotlin/no/nav/dagpenger/vedtaksmelding/model/klage/KlageMeldingOpprettholdelseTest.kt
+++ src/test/kotlin/no/nav/dagpenger/vedtaksmelding/model/klage/KlageMeldingOpprettholdelseTest.kt
@@ -3,6 +3,7 @@ package no.nav.dagpenger.vedtaksmelding.model.klage
 import io.kotest.matchers.shouldBe
 import no.nav.dagpenger.vedtaksmelding.model.klage.KlageBrevBlokker.KLAGE_OPPRETTHOLDELSE_DEL_1
 import no.nav.dagpenger.vedtaksmelding.model.klage.KlageBrevBlokker.KLAGE_OPPRETTHOLDELSE_DEL_2
+import no.nav.dagpenger.vedtaksmelding.model.klage.KlageMelding.Companion.fasteAvsluttendeBlokker
 import no.nav.dagpenger.vedtaksmelding.util.readFile
 import org.junit.jupiter.api.Test
 
@@ -22,7 +23,7 @@ class KlageMeldingOpprettholdelseTest {
                 listOf(
                     KLAGE_OPPRETTHOLDELSE_DEL_1.brevblokkId,
                     KLAGE_OPPRETTHOLDELSE_DEL_2.brevblokkId,
-                )
+                ) + fasteAvsluttendeBlokker
         }
     }
 }
