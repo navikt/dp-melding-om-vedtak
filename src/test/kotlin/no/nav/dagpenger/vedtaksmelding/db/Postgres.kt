b146ece9a6e5a7755e3b8109ad6ee7a502e43b54
Giao The Cung <giaothe@gmail.com>
2026-02-13 09:59:39 +0100

    Oppgradere Postgres til major versjon 18

--- src/test/kotlin/no/nav/dagpenger/vedtaksmelding/db/Postgres.kt
+++ src/test/kotlin/no/nav/dagpenger/vedtaksmelding/db/Postgres.kt
@@ -2,12 +2,12 @@ package no.nav.dagpenger.vedtaksmelding.db
 
 import com.zaxxer.hikari.HikariDataSource
 import org.flywaydb.core.internal.configuration.ConfigUtils
-import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.postgresql.PostgreSQLContainer
 import javax.sql.DataSource
 
 internal object Postgres {
     val instance by lazy {
-        PostgreSQLContainer<Nothing>("postgres:16.0").apply {
+        PostgreSQLContainer("postgres:18.1").apply {
             start()
         }
     }
